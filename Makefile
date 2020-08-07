0f4d2757f36625c2723b9584490a084ec1b40f82
liamdj23 <liamdj.9@gmail.com>
2020-08-07 11:53:55 +0200

    :construction_worker: Fix makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
-virtual: .venv/bin/pip 
+dependencies:
+	python3 -m pip install --user virtualenv
+virtual: .venv/bin/pip
 .venv/bin/pip:
-	virtualenv -p /usr/bin/python3 .venv
+	virtualenv -p python3 .venv
 install:
 	.venv/bin/pip install -Ur requirements.txt
 update-requirements: install
 	.venv/bin/pip freeze > requirements.txt
-.PHONY: virtual install
+.PHONY: dependencies virtual install
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Atoein-Discord-Bot",
+    version="0.0.1",
+    author="liamdj23",
+    author_email="me@liamdj23.ovh",
+    description="Great Discord Bot with 1k+ servers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liamdj23/Atorin-Discord-Bot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
