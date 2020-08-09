649ae0751e1a39d79e57a256b2af547b3df21f1b
liamdj23 <liamdj.9@gmail.com>
2020-08-09 22:51:35 +0200

    pls work

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Atoein-Discord-Bot",
+    name="Atorin-Discord-Bot",
     version="0.0.1",
     author="liamdj23",
     author_email="me@liamdj23.ovh",
