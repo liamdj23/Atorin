97722758e8ca92c2aa8a5f66833605f09eebee0c
liamdj23 <liamdj.9@gmail.com>
2020-08-13 20:56:36 +0200

    :sparkles: Added tvp command

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -1,5 +1,8 @@
 from discord.ext import commands
 import aiohttp
+from random import randrange
+from io import BytesIO
+import discord
 
 
 class Fun(commands.Cog):
@@ -13,3 +16,23 @@ class Fun(commands.Cog):
                 if r.status == 200:
                     data = await r.json()
                     await ctx.send(data[0])
+
+    @commands.command(usage="tekst")
+    async def tvp(self, ctx, *, text):
+        if len(text) > 48:
+            raise commands.BadArgument
+        async with aiohttp.ClientSession() as session:
+            async with session.post('https://pasek-tvpis.pl/index.php', data={"fimg": randrange(2), "msg": text}) as r:
+                if r.status == 200:
+                    image = await r.content.read()
+                    await ctx.send(file=discord.File(BytesIO(image), filename="tvp.png"))
+
+    @tvp.error
+    async def tvp_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("❌ Poprawne użycie: `&tvp <tekst>`")
+            return
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("❌ Zbyt duża ilość znaków, limit to 48 znaków.")
+            return
+        print("TVP: " + error)
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,5 +1,7 @@
 import distest
 import sys
+import random
+import string
 
 test_collector = distest.TestCollector()
 
@@ -12,6 +14,22 @@ async def test_ping(interface):
 @test_collector()
 async def test_shiba(interface):
     await interface.assert_reply_has_image("&shiba")
+
+
+@test_collector()
+async def test_tvp_no_argument(interface):
+    await interface.assert_reply_contains("&tvp", "❌ Poprawne użycie: `&tvp <tekst>`")
+
+
+@test_collector()
+async def test_tvp_bad_argument(interface):
+    random_text = ''.join(random.choices(string.ascii_letters, k=49))
+    await interface.assert_reply_contains("&tvp " + random_text, "❌ Zbyt duża ilość znaków, limit to 48 znaków.")
+
+
+@test_collector()
+async def test_tvp(interface):
+    await interface.assert_reply_has_image("&tvp test")
 
 if __name__ == '__main__':
     distest.run_dtest_bot(sys.argv, test_collector)
