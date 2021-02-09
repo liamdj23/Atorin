2c584da00f2e6fce23c711a4b80cd92f0193d890
liamdj23 <liamdj.9@gmail.com>
2021-02-09 23:01:29 +0100

    :bug: Repair github workflow

---
+++ config.js
@@ -0,0 +1,4 @@
+db.tokens.insert([
+    {"_id":"bot","key":"DISCORD_TOKEN"}, //DISCORD BOT TOKEN
+    {"_id":"weather","key":"WEATHER_TOKEN"} //OPEN WEATHER MAP API KEY
+])
--- mongo-init.js
+++
@@ -1,6 +0,0 @@
-import config from './config.json'
-
-db.tokens.insert([
-    {"_id":"bot","key":config.token}, //DISCORD BOT TOKEN
-    {"_id":"weather","key":config.weather} //OPEN WEATHER MAP API KEY
-])
