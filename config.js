eaf28742c735e0842830798a8583fc15b74f0c37
liamdj23 <liamdj.9@gmail.com>
2021-02-09 23:47:12 +0100

    :bug: Fixed typo

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-db.tokens.insert([
+print("########### START ###########");
+db = db.getSiblingDB("atorin");
+db.createCollection("token");
+db.token.insert([
     {"_id":"bot","key":"DISCORD_TOKEN"}, //DISCORD BOT TOKEN
     {"_id":"weather","key":"WEATHER_TOKEN"} //OPEN WEATHER MAP API KEY
-])
+]);
+print("########### END ###########");
