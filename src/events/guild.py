26300cb273e152ec5f001a6909a0975a2058c9fd
liamdj23 <liamdj.9@gmail.com>
2021-02-25 07:45:35 +0100

    :fire: Removed influx function

--- src/events/guild.py
+++ src/events/guild.py
@@ -4,22 +4,8 @@ class GuildEvents:
 
         @self.bot.event
         async def on_guild_join(guild):
-            update_guilds_stats(self.bot.influx, len(self.bot.guilds))
             await self.bot.update_status()
 
         @self.bot.event
         async def on_guild_remove(guild):
-            update_guilds_stats(self.bot.influx, len(self.bot.guilds))
             await self.bot.update_status()
-
-
-def update_guilds_stats(influx, value):
-    influx.write_points([{
-        "measurement": "servers",
-        "tags": {
-            "bot": "atorin"
-        },
-        "fields": {
-            "value": value
-        }
-    }])
