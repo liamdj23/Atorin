ff8f95aac6d9adf4f9ae3ccec4475e8a0b216dbf
liamdj23 <liamdj.9@gmail.com>
2021-04-23 14:21:29 +0200

    :sparkles: automatic assignment of a supporting rank

--- src/cogs/premium.py
+++ src/cogs/premium.py
@@ -6,6 +6,13 @@ class Premium(commands.Cog, name="💎 Premium"):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.guild.id == 408960275933429760:
+            if self.bot.mongo.Premium.objects(id=message.author.id).first():
+                supporting_role = discord.utils.get(message.guild.roles, id=807643329067876383)
+                await message.author.add_roles(supporting_role)
+
     @commands.command(description="Wysyła podaną wiadomość jako Atorin", usage="<tekst>", aliases=["echo"])
     async def say(self, ctx, *, content: commands.clean_content):
         if not self.bot.mongo.Premium.objects(id=ctx.author.id).first():
