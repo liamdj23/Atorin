8bac5ca3aaa5d18e10a9beb9e5a17f1de4d6e473
liamdj23 <liamdj.9@gmail.com>
2022-01-23 02:54:04 +0100

    :sparkles: Added duck command

--- atorin/commands/fun.py
+++ atorin/commands/fun.py
@@ -496,6 +496,24 @@ class Fun(commands.Cog, name="🎲 Zabawa"):
         embed.set_image(url=post["url"])
         await ctx.send_followup(embed=embed)
 
+    @slash_command(description="Losowe zdjęcie kaczki", guild_ids=config["guild_ids"])
+    async def duck(self, ctx: discord.ApplicationContext):
+        await ctx.defer()
+        while True:
+            r = requests.get(
+                "https://reddit.com/r/duck/random/.json",
+                headers={"User-agent": "Atorin"},
+            )
+            post = r.json()[0]["data"]["children"][0]["data"]
+            if post["url"].endswith(".jpg") or post["url"].endswith(".png"):
+                break
+        embed = discord.Embed()
+        embed.title = (
+            (post["title"][:200] + "...") if len(post["title"]) > 203 else post["title"]
+        )
+        embed.set_image(url=post["url"])
+        await ctx.send_followup(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
